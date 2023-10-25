fe49eedef45f6a5112618efa72455554b04d2bfe
Kadir Koç <kadiirkkoc@gmail.com>
2023-10-26 01:40:23 +0300

    first commit

---
+++ src/test/java/com/gendiary/GenDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gendiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
